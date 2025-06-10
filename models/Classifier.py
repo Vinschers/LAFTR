6d0b92d6ed99f59befe1268c09b49cdc4594c71f
Vinschers <code@a.vicentin.xyz>
2025-06-10 21:39:54 +0200

    Fix notebook and models
    Co-authored-by: marianaolm <marianaolm@outlook.com>

--- models/Classifier.py
+++ models/Classifier.py
@@ -17,4 +17,4 @@ class Classifier(nn.Module):
         self.net = nn.Linear(latent_dim, 1) # A single linear layer, no hidden layers or ReLU
 
     def forward(self, z):
-        return self.net(z).squeeze(-1)  # Squeeze is used to handle shape (batch_size, 1) -> (batch_size)
+        return self.net(z)
